e1f3162f6310d9f806171224632dfa2826a7a973
Ruslan Banochkin <ruslan@banochkin.com>
2020-01-23 18:49:46 +0300

    Initial project version

---
+++ database/migrations/2020_01_20_182826_create_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->uuid('uuid');
+            $table->string('filename');
+            $table->string('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
